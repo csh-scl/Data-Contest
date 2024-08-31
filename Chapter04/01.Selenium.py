13e681b7e097c5ad2c55e13abdb7169196ce6c85
Hyeon <csh-scl>
2024-08-31 22:03:33 +0900

    팝업창이 안 닫히는 문제 해결

--- Chapter04/01.Selenium.py
+++ Chapter04/01.Selenium.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)  # 옵션 추가
+# Chrome 드라이버 초기화
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.naver.com')
 
 driver.implicitly_wait(10)  # 10초 대기
@@ -18,11 +19,21 @@ shortcut_items = driver.find_elements(By.CLASS_NAME, 'shortcut_item')
 fourth_item = shortcut_items[3]
 fourth_item.find_element(By.CLASS_NAME, 'link_service').click()
 
-try:
-    continue_button = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="gnb-header"]/div/div[4]/div/div/button[2]'))
-    )
+# 창 전환
+main = driver.window_handles
+driver.switch_to.window(main[1])  # 여기가 수정된 부분입니다. 새 창으로 전환해야 합니다.
 
-    continue_button.click()  # 버튼 클릭하여 광고 닫기
+time.sleep(2)  # 페이지 로드 대기
+
+# 버튼 클릭
+try:
+    close_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button._targetBanner_button_1u8dJ._targetBanner_close_2F4H0'))
+    )
+    close_button.click()
 except Exception as e:
-    print(f"요소를 찾을 수 없습니다: {e}")
+    print(f"버튼 클릭 오류: {e}")
+
+# 드라이버 종료
+time.sleep(10)
+
